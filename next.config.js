5bf4315bc4010a3e2e43c439e60083b0373ec995
haraphat01 <haraphat@gmail.com>
2022-11-29 19:57:43 +0100

    Fixing the cors issue

--- next.config.js
+++ next.config.js
@@ -1,22 +1,41 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+// module.exports = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         headers: [
+//           { key: 'Access-Control-Allow-Credentials', value: 'true' },
+//           {
+//             key: 'Access-Control-Allow-Origin',
+//             value: 'https://https://pencilai.netlify.app',
+//           },
+//           {
+//             key: 'Access-Control-Allow-Methods',
+//             value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+//           },
+//           {
+//             key: 'Access-Control-Allow-Headers',
+//             value:
+//               'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
 module.exports = {
   async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'https://api.example.com/:path*',
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "https://https://pencilai.netlify.app" },
-            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ]
-        },
-      ]
-    },
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://https://pencilai.netlify.app/:path*',
+      },
+    ];
+  },
 };
